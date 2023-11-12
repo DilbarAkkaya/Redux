cc3fbab119e7256b7cf9c0305d65a2c4e1e103de
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-11-12 22:05:48 +0300

    feat: create func reducer

--- my-redux/src/index.js
+++ my-redux/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const initialState = 0;
+const reducer = (state, action)=>{
+switch (action.type) {
+  case 'INC':
+  return state + 1;
+  default:
+  return state;
+}
+}
+let state = reducer(initialState, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+console.log(state)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <></>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
